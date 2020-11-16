a629f9da2ee2289b236c2afe0e2eeef7b52129b3
dmonmad <davidmonmad@hotmail.com>
2020-11-16 12:34:49 +0100

    Attachments and comments

--- src/android/EmailSender2.java
+++ src/android/EmailSender2.java
@@ -10,7 +10,23 @@ import org.json.JSONObject;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.Multipart;
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.activation.DataHandler;
 import java.util.Properties;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
+
+
+import java.io.FileNotFoundException;
+import java.io.File;
+
 
 /**
  * This class echoes a string called from JavaScript.
@@ -31,7 +47,14 @@ public class EmailSender2 extends CordovaPlugin {
                         String to = args.getString(5);
                         String subject = args.getString(6);
                         String body = args.getString(7);
-                        sendEmail(ip, port, user, password, from, to, subject, body, callbackContext);
+                        List<String> attachments;
+                        if(args > 8){                        
+                            attachments = getListFromString(args.getString(8));
+                        }else{
+                            attachments = null;
+                        }
+
+                        sendEmail(ip, port, user, password, from, to, subject, body, attachments, callbackContext);
                     } catch (Exception e) {
                         callbackContext.error("VENGA YA TIO "+e.toString());
                     }
@@ -43,7 +66,7 @@ public class EmailSender2 extends CordovaPlugin {
         return true;
     }
 
-    private void sendEmail(String ip, String port, String user, String password, String from, String to, String subject, String body, CallbackContext callbackContext) {
+    private void sendEmail(String ip, String port, String user, String password, String from, String to, String subject, String body, List<String> attachments, CallbackContext callbackContext) {
         if (ip != null && ip.length() > 0) {
             if (port != null && port.length() > 0) {
                 if (user != null && user.length() > 0) {
@@ -52,11 +75,15 @@ public class EmailSender2 extends CordovaPlugin {
                             if (to != null && to.length() > 0) {
 
                                 try{
-                                    this.sendRealEmail(ip, port, user, password, from, to, subject, body);
+                                    this.sendRealEmail(ip, port, user, password, from, to, subject, body, attachments);
                                     callbackContext.success("Enviado!");
 
+                                }catch(FileNotFoundException e){                                    
+                                    callbackContext.error(e.toString());
+                                }catch(MessagingException e){                                    
+                                    callbackContext.error(e.toString());
                                 }catch(Exception e){                                    
-                                    callbackContext.error("JODER2 "+e.toString());
+                                    callbackContext.error(e.toString());
                                 }
                             } else {
                                 callbackContext.error("Destiny address is invalid.");
@@ -78,22 +105,28 @@ public class EmailSender2 extends CordovaPlugin {
         }
     }
 
-    public void sendRealEmail(String host, String port, String user, String password, String from, String to, String subject, String body)
-    throws MessagingException {
-        Properties prop = new Properties();
-        prop.put("mail.smtp.host", host);
-        prop.put("mail.smtp.port", port);
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "false"); //TLS
-                
-        Session session = Session.getInstance(prop,
-            new javax.mail.Authenticator() {
-                protected PasswordAuthentication getPasswordAuthentication() {
-                    return new PasswordAuthentication(user, password);
-            }
-        });
+    public void sendRealEmail(String host, String port, String user, String password, String from, String to, String subject, String body, List<String> attachments)
+    throws MessagingException, FileNotFoundException {
+        try{
+            MailcapCommandMap mc = (MailcapCommandMap) CommandMap.getDefaultCommandMap(); 
+            mc.addMailcap("text/html;; x-java-content-handler=com.sun.mail.handlers.text_html"); 
+            mc.addMailcap("text/xml;; x-java-content-handler=com.sun.mail.handlers.text_xml"); 
+            mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain"); 
+            mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed"); 
+            mc.addMailcap("message/rfc822;; x-java-content- handler=com.sun.mail.handlers.message_rfc822"); 
 
-        try {
+            Properties prop = new Properties();
+            prop.put("mail.smtp.host", host);
+            prop.put("mail.smtp.port", port);
+            prop.put("mail.smtp.auth", "true");
+            prop.put("mail.smtp.starttls.enable", "false"); //TLS
+                    
+            Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(user, password);
+                }
+            });
 
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(from));
@@ -104,13 +137,44 @@ public class EmailSender2 extends CordovaPlugin {
             message.setSubject(subject);
             message.setText(body);
 
+            if(attachments != null){
+                Multipart multipart = new MimeMultipart();
+                
+                for(int i = 0; i<attachments.size(); i++){
+
+                    MimeBodyPart messageBodyPart = new MimeBodyPart();
+                    String filename = attachments.get(i);
+                    DataSource source = new FileDataSource(filename);
+                    messageBodyPart.setDataHandler(new DataHandler(source));
+                    messageBodyPart.setFileName(getFileName(filename));
+                    multipart.addBodyPart(messageBodyPart);
+                }
+
+                message.setContent(multipart);
+            }          
+
             Transport.send(message);
 
-            System.out.println("Done");
-
         } catch (MessagingException e) {
-            throw new MessagingException("NO ME JDOAS "+e.toString());
+            throw new MessagingException(e.toString());
+        } catch (FileNotFoundException e){
+            throw new FileNotFoundException(e.toString());
         }
 
+    }
+
+    private List<String> getListFromString(String s) {
+        List<String> list = new ArrayList<String>();
+        String parts[] = s.split(",");
+        for(int i = 0; i<parts.length; i++){
+            list.add(parts[i]);
+        }
+        return list;
+    }
+
+    private String getFileName(String s) throws FileNotFoundException {
+        File f;
+        f = new File(s);
+        return f.getName();
     }
 }
--- www/EmailSender2.js
+++ www/EmailSender2.js
@@ -1,10 +1,25 @@
 var exec = require('cordova/exec');
 
 /**
- * EmailSender2 class
- * @constructor
+ * Sends an email
+ *
+ * Notice: As many ftp server could not rm dir when it's not empty, so always recommended to `rm` all files under the dir at first before `rmdir`.
+ *
+ * @param {string} ip IP or hostname of the SMTP server.
+ * @param {string} port Port of the SMTP server.
+ * @param {string} user User to connect with.
+ * @param {string} password Password to connect with.
+ * @param {string} from Email from where the email will be sent.
+ * @param {string} to Email where the email will be sent.
+ * @param {string} subject Subject of the email.
+ * @param {string} body Body of the email.
+ * @param {string[]} attachments The files that you want to send(from the private folder). e.g. "/storage/emulated/0/Android/data/io.ionic.starter/foo.pdf"..
+ * @param {function} successCallback The success callback. If triggered, means success.
+ * @param {function} errorCallback The error callback. If triggered, means fail.
  */
-
-exports.sendEmail = function (ip, port, user, password, from, to, subject, body, success, error) {
-    exec(success, error, 'EmailSender2', 'sendEmail', [ip, port, user, password, from, to, subject, body]);
+exports.sendEmail = function (ip, port, user, password, from, to, subject, body, attachments, success, error) {
+    if (attachments)
+        exec(success, error, 'EmailSender2', 'sendEmail', [ip, port, user, password, from, to, subject, body, attachments.toString()]);
+    else
+        exec(success, error, 'EmailSender2', 'sendEmail', [ip, port, user, password, from, to, subject, body, null]);
 };
